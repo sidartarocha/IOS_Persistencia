269fcff48a919c245625538bde3f5f17e470b170
Sidarta Rocha <sidarta_rocha@yahoo.com.br>
2021-12-28 23:22:34 -0300

    Cocoapods

--- Carangas/Carangas/Controllers/CarsTableViewController.swift
+++ Carangas/Carangas/Controllers/CarsTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SideMenu
 
 class CarsTableViewController: UITableViewController {
 
@@ -22,6 +23,20 @@ class CarsTableViewController: UITableViewController {
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(loadCars), for: .valueChanged)
         tableView.refreshControl = refreshControl
+        
+        
+        
+        // Define the menus
+        SideMenuManager.default.leftMenuNavigationController = storyboard!.instantiateViewController(withIdentifier: "LeftMenuNavigationController") as? SideMenuNavigationController
+       
+               
+        // Enable gestures. The left and/or right menus must be set up above for these to work.
+        // Note that these continue to work on the Navigation Controller independent of the View Controller it displays!
+               
+        SideMenuManager.default.addPanGestureToPresent(toView: self.navigationController!.navigationBar)
+        // Updated
+        SideMenuManager.default.addScreenEdgePanGesturesToPresent(toView: self.navigationController!.view, forMenu: SideMenuManager.PresentDirection.left)
+        
         
     }
     
